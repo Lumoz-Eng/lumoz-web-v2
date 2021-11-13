a3330c9d9b7b5cc088501db2703e4ddd42acc582
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-11-14 01:02:52 +0530

    add tech section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './brand.css'
+import { ai, webgl, nodejs, mediapipe, reactjs } from './import'
 
 const Brand = () => {
-  return <div>Brand</div>
+  return (
+    <div className='lumoz__brand section__padding'>
+      <div>
+        <img src={reactjs} alt='reactjs' />
+      </div>
+      <div>
+        <img src={ai} alt='ai' />
+      </div>
+      <div>
+        <img src={mediapipe} alt='mediapipe' />
+      </div>
+      <div>
+        <img src={nodejs} alt='nodejs' />
+      </div>
+      <div>
+        <img src={webgl} alt='webgl' />
+      </div>
+    </div>
+  )
 }
 
 export default Brand
---
+++ src/components/brand/import.js
@@ -0,0 +1,7 @@
+import ai from '../../assets/brands/googleai.png'
+import mediapipe from '../../assets/brands/mediapipe.png'
+import nodejs from '../../assets/brands/nodejs.png'
+import webgl from '../../assets/brands/webgl.png'
+import reactjs from '../../assets/brands/reactjs.png'
+
+export { ai, mediapipe, nodejs, webgl, reactjs }
