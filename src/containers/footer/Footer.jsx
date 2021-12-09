cc4a37a76b1358b0e7ed562fbf978a8431b6d8d7
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-12-09 21:02:18 +0530

    sm btn added

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -14,7 +14,14 @@ const Footer = () => (
     <div className='lumoz__footer-btn'>
       <p>Request Early Access</p>
     </div>
-
+    <div className='social-media-links'>
+      <a href='#' className='fa fa-facebook'></a>
+      <a href='#' className='fa fa-twitter'></a>
+      <a href='#' className='fa fa-google'></a>
+      <a href='#' className='fa fa-linkedin'></a>
+      <a href='#' className='fa fa-youtube'></a>
+      <a href='#' className='fa fa-instagram'></a>
+    </div>
     <div className='lumoz__footer-links'>
       <div className='lumoz__footer-links_logo'>
         <img src={lumozLogo} alt='lumoz_logo' />
