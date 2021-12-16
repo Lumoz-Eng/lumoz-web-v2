cbd3de26a42ada02241fec14f9f8f33cec238cd0
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-12-16 22:58:34 +0530

    demo gif's added

--- src/components/demo/Demo.jsx
+++ src/components/demo/Demo.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './demo.css'
 import demoImg from '../../assets/demo.gif'
+import demoImg2 from '../../assets/demo3.gif'
 
 const Demo = () => {
   return (
     <div className='demo_section section__padding'>
       <div className='demoimg_div'>
         <img src={demoImg} />
+        <img src={demoImg2} />
       </div>
     </div>
   )
