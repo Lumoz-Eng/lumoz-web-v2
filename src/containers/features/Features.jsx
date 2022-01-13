4db78c3134116b32da12b5dc250b58cd06a5cf73
Bawanthathilan <bawantharathnayaka@gmail.com>
2022-01-13 21:57:02 +0530

    added features content

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,19 +3,19 @@ import './features.css'
 import Feature from '../../components/feature/Feature'
 const featuresdata = [
   {
-    title: 'Improving end distrusts instantly',
+    title: '3D Object Placement ',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
   {
-    title: 'Improving end distrusts instantly',
+    title: 'Support Hand Gesture',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
   {
-    title: 'Improving end distrusts instantly',
+    title: 'Realtime Date visualization',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
   {
-    title: 'Improving end distrusts instantly',
+    title: 'IOT Device Support',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
 ]
@@ -28,7 +28,6 @@ const Features = () => {
           The Future is Now and You Just Need To Realize It. Step into Future
           Today & Make it Happen.
         </h1>
-        <p>Request Early Access to Get Started</p>
       </div>
       <div className='lumoz__features-container'>
         {featuresdata.map((item, index) => (
